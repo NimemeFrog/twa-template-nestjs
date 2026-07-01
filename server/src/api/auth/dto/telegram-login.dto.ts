4a56bef4c0cee8a5b0c8cb19ccc94862da18934a
GhostMan <fgoibfjd@gmail.com>
2026-07-01 14:37:10 +0400

    feat: Adding README.md & changes

--- server/src/api/auth/dto/telegram-login.dto.ts
+++ server/src/api/auth/dto/telegram-login.dto.ts
@@ -1,8 +1,9 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
 
 export class TelegramLoginDto
 {
     @IsString({ message: 'Field initData must be a string' })
     @IsNotEmpty({ message: 'Field initData must not be empty' })
+    @MaxLength(2000, { message: 'Длина initData превышает допустимую' })
     initData!: string;
 }
